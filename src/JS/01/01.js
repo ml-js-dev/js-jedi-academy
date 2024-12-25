0a1cbf5943cccd60d4a4847ffa345c9e27838def
Maxim Lee <maxim.lee@1win.pro>
2024-12-25 14:57:41 +0300

    lesson 2 & 3

--- src/JS/01/01.js
+++ src/JS/01/01.js
@@ -1,11 +1,104 @@
 //? IDE Setup
 
-//? Usage
+//? console
 
-//? Variables
+//? ПЕРЕМЕННЫЕ
 
-//? Data Types
+let variable //перезаписываемая переменная
+const variable1 = true
+let age = 11
+const test1 = true //константа
 
-//? Operators
+//? namingConventions
+//camelCase emailIsVerified
+//kebab-case email-is-verified
+//snake_case email_is_verified
+//PascalCase EmailIsVerified
 
-//? Console
+//? ТИПЫ ДАННЫХ
+//* Примитивные(По значению)
+const number = 42.6 //number often
+const bool = true || false //often
+const string = '1' //often
+const singleQuotes = "Hello's" //often
+let nullValue = null //often
+let undefinedValue = undefined //rare
+
+const bigInt = 7832567823456783456e6 //Big Int rare
+const symbol = Symbol('symbol') // almost never
+
+//* Ссылочные (Объекты)
+const object = {
+   name: 'John',
+   age: 15,
+   hobbies: {
+      sport: true,
+      reading: false,
+   },
+}
+
+
+//? КОММЕНТАРИИ
+// строчный комментарий
+/*
+БЛОЧНЫЙ КОММЕНТАРИЙ
+dsadfsadfsadf
+sadfasddfhfgdff
+const arr = []
+*/
+
+//? ОПЕРАТОРЫ
+//* Унарные
+// let increment = 5++ // => 6 increment
+// let decrement = 5-- // => 4 decrement
+
+//* Бинарные
+const addition = 2 + 2
+const subtraction = 5 - 2
+const multiplication = 5 * 2
+const division = 10 / 2
+// += , -=, *=, /=, %=, **=
+const exponentiation = 2 ** 3
+const modulus = 14 % 3
+
+//!Логические операторы приводящие к булеву true / false
+const equality = 5 === 5 //* Строгое сравнение - по значению и по типу данных
+const nonStringEquality = 5 == '5' //* Нестрогое сравнение - по значению, но не по типу данных COERCION "5" => value => value без кавычек это число? => yes => 5
+
+const inequality = 5 !== 5 СТРОГО НЕРАВНО false
+const nonStringInequality = 5 != '5' НЕСТРОГО НЕРАВНО
+
+const greaterThan = 5 > 3 
+const lessThan = 5 < 3
+const greaterThanOrEqual = 5 >= 5
+const lessThanOrEqual = 5 <= 3
+const logicalAnd = true && true 
+
+const objPolina = {
+   name: "Polina",
+   age: 25
+}
+
+(nameIs: Polina && ageIs: 25) => true => doSomething
+
+const logicalOr = false || false
+(nameIs: Polina || ageIs: 22) => true => doSomething
+(nameIs: Polina1 || ageIs: 25) => true => doSomething
+
+
+const logicalNot = !true
+
+const falsyArr = [false, 0, "", undefined, null, NaN ] => false
+
+//* Тернарный
+const ops = 10 === 11 ? "Hello" : "NotHello"
+
+//? ОПЕРАТОР typeof
+
+const typeOfNumber = typeof 5
+const typeOfString = typeof 'hello'
+const typeOfBoolean = typeof true
+const typeOfNull = typeof null
+const typeOfUndefined = typeof undefined
+const typeOfSymbol = typeof Symbol('symbol')
+const typeOfObject = typeof {}
---
+++ src/JS/02/02.js
@@ -0,0 +1,24 @@
+//? NUMBER VS STRING
+
+//* String
+let numberString = '1'
+
+//* Number
+let number = 1
+// not the same
+
+//? COMBINING STRINGS
+//* CONCATINATION
+
+let data = 'Hello my name is ' + 'Polina'
+let firstPart = 'Hello my name is '
+let secondPart = 'Polina'
+let concat = firstPart + 'Vika'
+let woSpaces = 'Hello' + 'new' + 'world'
+
+//? COMBINING STRINGS AND NUMBER
+let combo = '1' + 1 //'11'
+// 1 + '1' = 11
+
+//?? FALSY VALUES
+const falsyVals = [false, 0, '', null, undefined, NaN]
